fd66076812e875f25207ce1b818afa132a223122
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-05 10:27:54 -0500

    adding greeting component

--- myapp-display/src/App.jsx
+++ myapp-display/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+
+import GreetingDisplay from './GreetingComponent.Jsx';
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>TESTING</h1>
+      
+      <GreetingDisplay />
+
     </>
   )
 }
 
-export default App
+export default App;
+
